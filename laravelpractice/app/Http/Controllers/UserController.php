318929631196c82b874fb2d9bd0e2dbee12baf7d
abudhosamuel <abudhosamuel@gmail.com>
2025-03-09 16:12:32 +0300

    use of controllers and views

---
+++ laravelpractice/app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function register(Request $request) {
+        $incomingFields = $request->validate(['name'=>'required','email'=> 'required','password'=> 'required']);
+        return 'Hello from our controller';
+    }
+}
